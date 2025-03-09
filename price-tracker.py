8da931d5d3dddef2b8b6ba1ad92534585b30b149
ljaviertovar <luisjavier_tovar@outlook.com>
2025-03-09 14:15:45 -0400

    update tracker

--- price-tracker.py
+++ price-tracker.py
@@ -5,29 +5,39 @@ import re
 from datetime import datetime
 from bs4 import BeautifulSoup
 
+
 class PriceTracker:
     def __init__(self, tracked_products="tracked_products.json"):
         self.tracked_products = tracked_products
 
     common_prices_selectors = [
-      '.price', '.product-price', '.offer-price', '.current-price', '[itemprop="price"]', '.price_value', '.price-current', '.a-price .a-offscreen', '#priceblock_ourprice', '#priceblock_dealprice', '.andes-money-amout_fraction'
+        ".price",
+        ".product-price",
+        ".offer-price",
+        ".current-price",
+        '[itemprop="price"]',
+        ".price_value",
+        ".price-current",
+        ".a-price .a-offscreen",
+        "#priceblock_ourprice",
+        "#priceblock_dealprice",
+        ".andes-money-amout_fraction",
     ]
 
     def format_thousand_separator(self, price_text, thousand_separator):
-      if thousand_separator == '.':
-        price_text = price_text.replace('.', '')
-        price_text = price_text.replace(',', '.')
-      else:
-        price_text = price_text.replace(',', '')
+        if thousand_separator == ".":
+            price_text = price_text.replace(".", "")
+            price_text = price_text.replace(",", ".")
+        else:
+            price_text = price_text.replace(",", "")
 
-      return price_text
+        return price_text
 
     def clean_price(self, price_text):
-      clean_price = re.sub(r'[^\d.]', '', price_text)
-      match = re.search(r'\d+\.\d+|\d+', clean_price)
+        clean_price = re.sub(r"[^\d.]", "", price_text)
+        match = re.search(r"\d+\.\d+|\d+", clean_price)
 
-      return float(match.group()) if match else None
-
+        return float(match.group()) if match else None
 
     def get_price(self, url, selector=None, thousand_separator=","):
         """Get the price of a product from a given URL"""
@@ -35,7 +45,7 @@ class PriceTracker:
         try:
             headers = {
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-                "acept-language": "en-US,en;q=0.5"
+                "acept-language": "en-US,en;q=0.5",
             }
 
             response = requests.get(url, headers=headers, timeout=10)
@@ -45,15 +55,14 @@ class PriceTracker:
 
             soup = BeautifulSoup(response.text, "html.parser")
 
-            # If a selector is provided, use it to get the price
             price_el = None
             if selector:
                 price_el = soup.select_one(selector)
             else:
                 for selector in self.common_prices_selectors:
-                  price_el = soup.select_one(selector)
-                  if price_el:
-                    break
+                    price_el = soup.select_one(selector)
+                    if price_el:
+                        break
 
             if not price_el:
                 return None
@@ -64,5 +73,58 @@ class PriceTracker:
             return self.clean_price(price_text)
 
         except Exception as e:
-          print(f"Error getting price: {e}")
-          return None
+            print(f"Error getting price: {e}")
+            return None
+
+    def load_tracked_products(self):
+        """Load the tracked products from the JSON file"""
+        if not os.path.exists(self.tracked_products):
+            return []
+
+        with open(self.tracked_products, "r", encoding="utf-8") as file:
+            return json.load(file)
+
+    def save_tracked_products(self, products):
+        """Save the tracked products to the JSON file"""
+        try:
+            with open(self.tracked_products, "w", encoding="utf-8") as file:
+                json.dump(products, file, indent=4)
+
+            return True
+        except Exception as e:
+            print(f"Error saving tracked products: {e}")
+            return False
+
+    def add_product(
+        self, name, url, desired_price, selector=None, thousand_separator=","
+    ):
+        """Add a product to the tracked products"""
+        products = self.load_tracked_products()
+
+        # Check if the product is already being tracked
+        for product in products:
+            if product["url"] == url:
+                return False
+
+        actual_price = self.get_price(url, selector, thousand_separator)
+
+        # Check if the price could be obtained
+        if actual_price is None:
+            return False
+
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        products.append({
+            "name": name,
+            "url": url,
+            "desired_price": desired_price,
+            "actual_price": actual_price,
+            "selector": selector,
+            "thousand_separator": thousand_separator,
+            "created_at": date,
+            "price_history": [{
+              "date": date,
+              "price": actual_price
+            }],
+        })
+
+        self.save_tracked_products(products)
