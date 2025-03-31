3eea85ba52c8e8d8aed74fe85847cf6e7a87acf8
ljaviertovar <luisjavier_tovar@outlook.com>
2025-03-31 10:19:51 -0400

    update workfolw

--- update_prices.py
+++ update_prices.py
@@ -2,12 +2,12 @@ from price_tracker import PriceTracker
 
 tracker = PriceTracker()
 
-print("\nUpdating prices...")
+print("\n=> Updating prices...")
 
 updated_prices = tracker.update_prices()
 if updated_prices:
     print(
-        f"\n{len(updated_prices)} product(s) have reached the desired price! :D\n",
+        f"\n\033[00m{len(updated_prices)} product(s) have reached the desired price! :D\033[00m\n",
     )
 
     for product in updated_prices:
@@ -15,4 +15,4 @@ if updated_prices:
             f"PRODUCT: {product['name']} - NEW CURRENT PRICE: {product['current_price']}"
         )
 else:
-    print("\nNo products have reached the disired price.")
+    print("\n\033[93mNo products have reached the disired price.\033[00m\n")
