33eafcb9ce34f7cd2aecaa1fcdbd784112b26e09
ljaviertovar <luisjavier_tovar@outlook.com>
2025-03-31 10:40:48 -0400

    update workfolw

--- update_prices.py
+++ update_prices.py
@@ -1,20 +1,21 @@
 from price_tracker import PriceTracker
-from utils import log_message
+from rich.console import Console
+
+console = Console()
 
 tracker = PriceTracker()
 
-print("\n=> Updating prices...")
+with console.status("\nUpdating prices...") as staus:
 
-updated_prices = tracker.update_prices()
-if updated_prices:
-    log_message(
-        f"\n{len(updated_prices)} product(s) have reached the desired price! :D\n",
-        " success",
-    )
-
-    for product in updated_prices:
-        print(
-            f"PRODUCT: {product['name']} - NEW CURRENT PRICE: {product['current_price']}"
+    updated_prices = tracker.update_prices()
+    if updated_prices:
+        console.log(
+            f"\n[green]{len(updated_prices)} product(s) have reached the desired price! :D[/green]\n"
         )
-else:
-    log_message("\nNo products have reached the disired price.", "warning")
+
+        for product in updated_prices:
+            print(
+                f"PRODUCT: {product['name']} - NEW CURRENT PRICE: {product['current_price']}"
+            )
+    else:
+        console.log("\n[yellow]No products have reached the disired price.[/yellow]\n")
